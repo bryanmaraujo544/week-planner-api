378a57c675b8d226c70db81bdea3f54ce22cb9e8
Bryan Martins <bryanmaraujo544@gmail.com>
2021-12-06 18:37:52 -0300

    fix: fixing database infos

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const db = mysql.createPool({
-    host: 'locahost',
+    host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'week_planner'
